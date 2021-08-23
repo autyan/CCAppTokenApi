c9d8f7cce7cb5a4f62f0a54f4fb47536709a2f92
Alex <autyan@outlook.com>
2021-08-23 13:52:18 +0800

    更新接口返回值类型

--- CCAppTokenApi/Controllers/TokenController.cs
+++ CCAppTokenApi/Controllers/TokenController.cs
@@ -1,8 +1,10 @@
 ﻿using CCAppTokenApi.Extensions;
+using CCAppTokenApi.Models;
 using CCAppTokenApi.Requests;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace CCAppTokenApi.Controllers
@@ -12,17 +14,18 @@ namespace CCAppTokenApi.Controllers
     public class TokenController : ControllerBase
     {
         [HttpPost]
-        public async Task<string> Post([FromBody] JwtTokenRequest request)
+        public async Task<JwtToken> Post([FromBody] JwtTokenRequest request)
         {
             var client = new HttpClient
                          {
                              BaseAddress = new Uri("http://apic.weiincloud.com/CC/token")
                          };
             var dict = request.ToDictionary();
-            var req    = new HttpRequestMessage(HttpMethod.Post, "http://apic.weiincloud.com/CC/token") { Content = new FormUrlEncodedContent(dict) };
-            var res    = await client.SendAsync(req);
-            var response = await res.Content.ReadAsStringAsync();
-            return response;
+            var requestMessage    = new HttpRequestMessage(HttpMethod.Post, "http://apic.weiincloud.com/CC/token") { Content = new FormUrlEncodedContent(dict) };
+            var response    = await client.SendAsync(requestMessage);
+            var jsonStream = await response.Content.ReadAsStreamAsync();
+            var tokenInfo = await JsonSerializer.DeserializeAsync<JwtToken>(jsonStream);
+            return tokenInfo;
         }
     }
 }
---
+++ CCAppTokenApi/Models/JwtToken.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CCAppTokenApi.Models
+{
+    public class JwtToken
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public uint ExpiresIn { get; set; }
+
+        [JsonPropertyName("refresh_token")]
+        public string RefreshToken { get; set; }
+
+        [JsonPropertyName("as:client_id")]
+        public string AsClientId { get; set; }
+
+        [JsonPropertyName(".issued")]
+        public string Issued { get; set; }
+
+        [JsonPropertyName(".expires")]
+        public string Expires { get; set; }
+    }
+}
